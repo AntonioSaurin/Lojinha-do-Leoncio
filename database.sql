8f74ceb18078229ed67b5fbce8c754ac4790f08f
AntonioSaurin <antonio.saurin@etec.sp.gpv.br>
2024-04-29 18:52:45 -0300

    Inserts Basicos

--- database.sql
+++ database.sql
@@ -22,4 +22,9 @@ primary key(id)
 
 insert into `users` (`username`, `password`) values ('Leoncio', 'Admin');
 
-select * from itens;
+insert into `itens` (`description`, `value`, `role`) values('Eclipse','2800','Assassin');
+insert into `itens` (`description`, `value`, `role`) values('Armadura de Warmog','3100','Tank');
+insert into `itens` (`description`, `value`, `role`) values('Cutelo Negro','3000','Bruiser');
+insert into `itens` (`description`, `value`, `role`) values('Força Do Vendaval','3400','ADCarry');
+insert into `itens` (`description`, `value`, `role`) values('Companheiro de Luden','2900','Mage');
+insert into `itens` (`description`, `value`, `role`) values('Cajado Aquafluxo','2300','Support');
